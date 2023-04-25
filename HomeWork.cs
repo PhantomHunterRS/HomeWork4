812f842984846447b7c43a598170c2c8b0149cbf
Alexey Filyakov <ASerFilyakov@mail.ru>
2023-04-26 00:31:52 +0300

    workin6

--- HomeWork.cs
+++ HomeWork.cs
@@ -1,36 +1,45 @@
-﻿internal class HomeWork
+﻿using System.Threading.Tasks;
+
+internal class HomeWork
 {
     private static void Main(string[] args)
     {
         HomeWork x = new HomeWork();
-        //Task 25
-        Console.WriteLine("Input two numbers.the first number to be raised, the second number to what degree.separated by commas");
-        int[] inputNumbers = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
-        Console.WriteLine(x.mathPows(inputNumbers[0], inputNumbers[1]));
-        //Task27
-        Console.WriteLine("Input a number to calculate the amount");
-        string number = Console.ReadLine();
-        Console.WriteLine(x.summNumbers(number));
-        //Task29
-        Console.WriteLine("Input a 8 numbers separated by commas");
-        string numberStr = Console.ReadLine();
-        int[] arrNumbers = x.arrayNumbers(numberStr);
-        x.arrNumb(arrNumbers);
+        ////Task 25
+        //Console.WriteLine("Input two numbers.the first number to be raised, the second number to what degree.separated by commas");
+        //int[] inputNumbers = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+        //Console.WriteLine(x.mathPows(inputNumbers[0], inputNumbers[1]));
+        ////Task27
+        //Console.WriteLine("Input a number to calculate the amount");
+        //string number = Console.ReadLine();
+        //Console.WriteLine(x.summNumbers(number));
+        ////Task29
+        //Console.WriteLine("Input a 8 numbers separated by commas");
+        //string numberStr = Console.ReadLine();
+        //int[] arrNumbers = x.arrayNumbers(numberStr);
+        //x.arrNumb(arrNumbers);
 
-        // Домашнее задание 5
-        HomeWork5 y = new HomeWork5();
-        //Task34
-        int[] ints34 = y.fillingTheArray();
-        x.arrNumb(ints34);
-        Console.WriteLine(y.foundEvenNumbers(ints34));
-        //Task36
-        int[] ints36 = y.fillingTheArray2();
-        x.arrNumb(ints36);
-        Console.WriteLine(y.summOfOddElements(ints36));
-        //Task38
-        double[] ints38 = y.fillingTheArray3();
-        x.arrNumb(ints38);
-        Console.WriteLine(y.findMinMaxRezult(ints38));
+        //// Домашнее задание 5
+        //HomeWork5 y = new HomeWork5();
+        ////Task34
+        //int[] ints34 = y.fillingTheArray();
+        //x.arrNumb(ints34);
+        //Console.WriteLine(y.foundEvenNumbers(ints34));
+        ////Task36
+        //int[] ints36 = y.fillingTheArray2();
+        //x.arrNumb(ints36);
+        //Console.WriteLine(y.summOfOddElements(ints36));
+        ////Task38
+        //double[] ints38 = y.fillingTheArray3();
+        //x.arrNumb(ints38);
+        //Console.WriteLine(y.findMinMaxRezult(ints38));
+
+        //// Домашнее задание 6
+        HomeWork6 hw6 = new HomeWork6();
+        //// Task41
+        //Console.WriteLine(hw6.countingPositiveNumbers(hw6.inputNumbers()));
+        // Task 43
+        x.arrNumb(hw6.findTheIntersectionPoint(hw6.inputValues()));
 
     }
 
---
+++ HomeWork6.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+internal class HomeWork6
+    {
+    // Task41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+    public int[] inputNumbers()
+    {
+        Console.WriteLine("Enter real numbers separated by a space");
+        int[] arrayLine = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+        return arrayLine;
+    }
+     public int countingPositiveNumbers(int[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Task 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+    // заданных уравнениями y = k1* x + b1, y = k2* x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+    public int[] inputValues()
+    {   int[] values = new int[4];
+        bool cirle = true;
+        while (cirle) {
+            Console.WriteLine("Enter values b1, k1 to create a direct y = k1* x + b1 separated by a space");
+            values = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            Console.WriteLine("Enter values b2, k2 to create a direct y = k2* x + b2 separated by a space");
+            values = values.Concat(Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse)).ToArray();
+            if (values[0] != values[2])
+            {
+                cirle = false;
+            }
+            else
+            {
+                Console.WriteLine("Straight lines don't intersect. they are parallel");
+            }
+        }
+        return values;
+    }
+    public double[] findTheIntersectionPoint(int[] values)
+    {
+        double[] point = new double[2];
+        point[0] = (values[3] - values[1]) / (double)(values[0] - values[2]);
+        point[1] = (values[2] * ((values[3] - values[1]) / (double)(values[0] - values[2])) + values[2]);
+        return point;
+     }
+
+}
